f66703630000561d6858a677aeea04a00eb55447
azlux <azlux@outlook.com>
2016-04-18 14:07:20 +0200

    sudo check

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-cp log2ram.service /etc/systemd/system/log2ram.service
-cp log2ram /usr/local/bin/log2ram
-systemctl enable log2ram
-
-echo "Reboot to activate log2ram"
+if [ `id -u` -eq 0 ]
+then
+  cp log2ram.service /etc/systemd/system/log2ram.service
+  cp log2ram /usr/local/bin/log2ram
+  systemctl enable log2ram
+  
+  echo "Reboot to activate log2ram"
+else 
+  echo "You need to be ROOT (sudo can be used)"
+fi
