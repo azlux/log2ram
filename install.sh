48980b2de2351c72a3287b73b1b0a98ca4c639fc
azlux <azlux@outlook.com>
2016-04-18 14:12:30 +0200

    I miss all chmod

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 if [ `id -u` -eq 0 ]
 then
   cp log2ram.service /etc/systemd/system/log2ram.service
+  chmod a+x /etc/systemd/system/log2ram.service
   cp log2ram /usr/local/bin/log2ram
+  chmod a+x /usr/local/bin/log2ram
   systemctl enable log2ram
   
   echo "Reboot to activate log2ram"
