918eff6385ea318fba7aba6d40ab209243f39baf
StuartIanNaylor <stuartiannaylor@outlook.com>
2019-03-07 05:07:58 +0000

    Remove console output

--- crontab.sh
+++ crontab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 crontab -l > mycron
-echo '* * * * * /usr/local/bin/log2ram-scheduler 2>&1 | /usr/bin/logger -t log2ram-scheduler' >> mycron
+echo '* * * * * /usr/local/bin/log2ram-scheduler 2>&1 /dev/null' >> mycron
 crontab mycron
 rm mycron
