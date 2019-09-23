12662bc6ee23d014bc096d67a7976a4bf0f72fac
MegaV0lt <MegaV0lt@users.noreply.github.com>
2019-09-23 15:52:35 +0200

    Update install.sh
    try to detect init-system and install accordingly

--- install.sh
+++ install.sh
@@ -1,18 +1,43 @@
 #!/usr/bin/env sh
 
-systemctl -q is-active log2ram  && { echo "ERROR: log2ram service is still running. Please run \"sudo service log2ram stop\" to stop it."; exit 1; }
 [ "$(id -u)" -eq 0 ] || { echo "You need to be ROOT (sudo can be used)"; exit 1; }
 
+# See if we can find out the init-system
+echo "Try to detect init..."
+if [ "$(systemctl --version)" != '' ] ; then
+  INIT='systemd'
+elif [ "$(rc-service --version)" != '' ] ; then
+  INIT='openrc'
+fi
+
+case "$INIT" in
+  systemd)
+    systemctl -q is-active log2ram  && { echo "ERROR: log2ram service is still running. Please run \"sudo service log2ram stop\" to stop it."; exit 1; } ;;
+  openrc)
+    rc-service log2ram status  && { echo "ERROR: log2ram service is still running. Please run \"sudo rc-service log2ram stop\" to stop it."; exit 1; } ;;
+  *) echo 'ERROR: could not detect init-system' ; exit 1
+  ;;
+esac
+  
 # log2ram
 mkdir -p /usr/local/bin/
-install -m 644 log2ram.service /etc/systemd/system/log2ram.service
 install -m 755 log2ram /usr/local/bin/log2ram
 install -m 644 log2ram.conf /etc/log2ram.conf
 install -m 644 uninstall.sh /usr/local/bin/uninstall-log2ram.sh
-systemctl enable log2ram
+if [ "$INIT" = 'systemd' ] ; then
+  install -m 644 log2ram.service /etc/systemd/system/log2ram.service
+  systemctl enable log2ram
+elif [ "$INIT" = 'openrc' ] ; then
+  install -m 755 log2ram.initd /etc/init.d/log2ram
+  rc-update add log2ram boot
+fi
 
 # cron
-install -m 755 log2ram.cron /etc/cron.daily/log2ram
+if [ "$INIT" = 'systemd' ] ; then
+  install -m 755 log2ram.cron /etc/cron.daily/log2ram
+elif [ "$INIT" = 'openrc' ] ; then
+  install -m 755 log2ram.openrc_cron /etc/cron.daily/log2ram
+fi
 install -m 644 log2ram.logrotate /etc/logrotate.d/log2ram
 
 # Remove a previous log2ram version
