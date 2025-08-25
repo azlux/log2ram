213f71233c44e3f0a06a0c9772822b20fb943f72
XhmikosR <xhmikosr@gmail.com>
2025-08-25 07:41:48 +0300

    Assorted consistency tweaks

--- log2ram-cron.sh
+++ log2ram-cron.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+
 #
 # cron.daily/log2ram -- daily write/sync ramlog to disk
 #
 #
-LOG2RAM_SCRIPT=/usr/bin/log2ram
-CONFIG=/etc/log2ram.conf
-# Check if all is fine with the needed files
-[ -f $CONFIG ] || exit 1
-[ -x $LOG2RAM_SCRIPT ] || exit 1
-cat /proc/mounts | grep -w log2ram >/dev/null || {
-    echo -ne "log2ram is NOT running\n"
-    exit 1
-}
 
-exec ${LOG2RAM_SCRIPT} write
+LOG2RAM_SCRIPT=/usr/bin/log2ram
+LOG2RAM_CONFIG=/etc/log2ram.conf
+
+# Check if all is fine with the needed files
+if [[ ! -f "${LOG2RAM_CONFIG}" ]]; then
+    echo "${LOG2RAM_CONFIG} not found"
+    exit 1
+fi
+
+if [[ ! -x "${LOG2RAM_SCRIPT}" ]]; then
+    echo "${LOG2RAM_SCRIPT} not executable"
+    exit 1
+fi
+
+if ! grep -w log2ram /proc/mounts >/dev/null; then
+    echo "log2ram is NOT running"
+    exit 1
+fi
+
+exec "${LOG2RAM_SCRIPT}" write
